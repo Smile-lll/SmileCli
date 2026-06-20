d94be05e03906bb7e9b10a2e029ae5332a9d95bd
Smile <1176250197@qq.com>
2026-06-20 15:52:32 +0800

    创建项目

---
+++ src/main/java/edu/sdu/App.java
@@ -0,0 +1,13 @@
+package edu.sdu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
